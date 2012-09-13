9a103d42027dfd654af6b49c0ffb912ec20cd28b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-09-13 20:55:06 +0100

    mister manager should start and stop a service

--- lib/mister/manager.rb
+++ lib/mister/manager.rb
@@ -1,4 +1,14 @@
 module Mister
   class Manager
+
+    PORT=9876
+    def initialize(address)
+      @service = Mister::Service.new(address, PORT, self)
+      @service.start
+    end
+
+    def finalize
+      @service.stop
+    end
   end
 end
